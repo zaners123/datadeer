440429b24a2c45a373fe8435de851f6a46e46567
zaners123 <zaners12345@gmail.com>
2020-09-15 17:44:30 -0700

    Push

--- html/21/index.php
+++ html/21/index.php
@@ -3,23 +3,15 @@
 <?php require "/var/www/php/bodyTop.php"; ?>
 
 <h1>Welcome to "21"</h1>
-
-<div>
-	If you're reading this it means you were curious enough to wonder what I put here... Well, here's a list
-</div>
-
+<div>If you're reading this it means you were curious enough to wonder what I put here... Well, here's a list</div>
 <hr>
-
 <div>
 	<a href="open">
 		Education on socially-disputed topics.
 		This is mostly revolving around having an open mind and not hating something until you know about it.
 	</a>
 </div>
-
-<div>
-	<a href="fortune.php">Fortunes (but more offensive)</a>
-</div>
-<div>
-<!--	<a href="https://pornhub.com">Literally just a link to pornhub</a>-->
-</div>
+<hr>
+<div><a href="fortune.php">Fortunes (but more offensive)</a><div>
+<hr>
+<div><a href="dictionary.php">The Furry Dictionary</a><div>
